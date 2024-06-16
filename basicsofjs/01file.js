495758527544e616233f68324dbf1133155d820f
GaganSinghal1234 <125457228+GaganSinghal1234@users.noreply.github.com>
2024-06-16 09:43:38 +0000

    datatypes

---
+++ basicsofjs/01file.js
@@ -0,0 +1,30 @@
+console.log("gagan");
+
+const accountId =144553
+let accountEmail ="gagansinghal@gmail.com"
+var pass ="254613"
+city ="ghaziabad"
+console.log(accountId);
+/* not use var due to scopping problem in js*/
+
+console.table([accountId,accountEmail,pass,city])
+/* we use give veriable type for any variable but it can also work , we prefer to give value to variable othervise it give undefine*/
+// we don't
+//"use strict"
+//alert()
+/*treat all code a newer verision of js,
+alert is not work now bcoz we use node not the browser*/
+
+//datatype in js
+/* 
+//sixtype
+number
+bigint
+string
+boolean
+null
+undefined
+symbol*/
+
+//colsole.log(typeof null)//object
+
