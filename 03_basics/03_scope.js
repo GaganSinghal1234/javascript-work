8908a0660dabc708480d1d906657ed20e154a89c
GaganSinghal1234 <gagansinghal1806@gmail.com>
2025-04-09 17:15:21 +0530

    scope

---
+++ 03_basics/03_scope.js
@@ -0,0 +1,12 @@
+//use const and let variable 
+//dont use var , dont use variable without type 
+
+// {
+//     block scope
+// }
+
+
+
+// global scope all
+
+/// browser global scope is different from node global scope
