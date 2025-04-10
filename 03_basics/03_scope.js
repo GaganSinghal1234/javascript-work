1bf857bbfa4ee4620ed0856264461be43e69140c
GaganSinghal1234 <gagansinghal1806@gmail.com>
2025-04-10 11:57:55 +0530

    scope

--- 03_basics/03_scope.js
+++ 03_basics/03_scope.js
@@ -10,3 +10,27 @@
 // global scope all
 
 /// browser global scope is different from node global scope
+
+
+function one(){
+    const username ="gagan";
+    function two (){
+        const website ="yoout";
+        console.log(username);
+
+    }
+    two();
+}
+one();
+
+
+
+if (true){
+    const username ="gagan";
+    if(username === "gagan"){
+        const website ="routout";
+        console.log(username , website);
+    }
+    // console.log(website);
+
+}
